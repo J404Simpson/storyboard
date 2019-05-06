ed0ed71f9a86e2d79ede1ae3da2b239e21a98f83
Jason J Simpson <jasonjamessimpson@gmail.com>
2019-05-06 17:32:08 +1200

    Added second sentence to seed data for testing purposes

--- server/db/seeds/sentences.js
+++ server/db/seeds/sentences.js
@@ -4,7 +4,8 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('sentences').insert([
-        {id: 1, sentence: 'Once upon a time'}
+        {id: 1, sentence: 'Once upon a time. '},
+        {id: 2, sentence: 'In the beautiful land of Aotearoa. '}
       ])
     })
 }
